dc032773751f566afc9be8912c683cfaa32aa413
SIBHU <123748142+sibhu-88@users.noreply.github.com>
2025-03-31 15:09:23 +0530

    Update show.c

--- RFID_STUDENTS_RECORD/show.c
+++ RFID_STUDENTS_RECORD/show.c
@@ -1,54 +1,49 @@
 #include "header.h"
 
+#define HEADER_ROW "\t+--------------+---------+---------------------+-------------+\n"
+#define STUDENT_HEADER "\t| RFID         | Roll.No | Name                | Percentage  |\n"
+
 void display_students_list(SDB *ptr) {
-    if (ptr == NULL) {
+    if (!ptr) {
         printf("\n\tNo records found.\n");
         return;
     }
 
-    printf("\n");
-    printf("\t+--------------+---------+---------------------+-------------+\n");
+    printf("\n%s", HEADER_ROW);
     printf("\t|                 STUDENTS LIST                              |\n");
-    printf("\t+--------------+---------+---------------------+-------------+\n");
-    printf("\t| RFID         | Roll.No | Name                | Percentage  |\n");
-    printf("\t+--------------+---------+---------------------+-------------+\n");
+    printf("%s", HEADER_ROW);
+    printf("%s", STUDENT_HEADER);
+    printf("%s", HEADER_ROW);
 
-    while (ptr != NULL) {
-        printf("\t| %-12s | %-7d | %-19s | %-11.2f |\n", ptr->RFID, ptr->rollno, ptr->name, ptr->percentage);
+    while (ptr) {
+        printf("\t| %-12s | %-7d | %-19s | %-11.2f |\n", 
+                ptr->RFID, ptr->rollno, ptr->name, ptr->percentage);
         ptr = ptr->next;
     }
-    printf("\t+--------------+---------+---------------------+-------------+\n\n");
-
+    printf("%s\n", HEADER_ROW);
 }
 
 int display_student_list(SDB *ptr, char *RFID) {
-    if (ptr == NULL) {
+    if (!ptr) {
         printf("\n\tNo records found.\n");
         return 0;
     }
 
-    
-    int found = 0;
-    while (ptr != NULL) {
+    while (ptr) {
         if (strncmp(ptr->RFID, RFID, 12) == 0) {
-		printf("\n");
-		printf("\t+---------+---------------------+-------------+\n");
-		printf("\t|               STUDENT Record                |\n");
-		printf("\t+---------+---------------------+-------------+\n");
-		printf("\t| Roll.No | Name                | Percentage  |\n");
-		printf("\t+---------+---------------------+-------------+\n");
-		printf("\t| %-7d | %-21s | %-11.2f |\n", ptr->rollno, ptr->name, ptr->percentage);
-		printf("\t+---------+---------------------+-------------+\n\n");
-            found = 1;
-	    break;
+            printf("\n%s", HEADER_ROW);
+            printf("\t|               STUDENT RECORD                |\n");
+            printf("%s", HEADER_ROW);
+            printf("\t| Roll.No | Name                | Percentage  |\n");
+            printf("%s", HEADER_ROW);
+            printf("\t| %-7d | %-21s | %-11.2f |\n", 
+                    ptr->rollno, ptr->name, ptr->percentage);
+            printf("%s\n", HEADER_ROW);
+            return 1; // Found
         }
         ptr = ptr->next;
     }
 
-    if (!found) {
-        printf("\n\n\tNo matching records found for RFID: %s \n\n", RFID);
-	return 0;
-    }
-	return 1;
-
+    printf("\n\n\tNo matching records found for RFID: %s \n\n", RFID);
+    return 0;
 }
