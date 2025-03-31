9a964b96acd481b7ffc9b094e601854afdd2003c
SIBHU <123748142+sibhu-88@users.noreply.github.com>
2025-03-31 15:02:54 +0530

    Update main.c

--- RFID_STUDENTS_RECORD/main.c
+++ RFID_STUDENTS_RECORD/main.c
@@ -1,110 +1,52 @@
 #include "header.h"
 
-#define SCREEN_WIDTH 80 // Define the width of the terminal screen
-
-void print_centered(const char *str) {
-    int len = strlen(str);
-    int padding = (SCREEN_WIDTH - len) / 2;
-    if (padding > 0) {
-        printf("%*s%s\n", padding, "", str);
-    } else {
-        printf("%s\n", str);
-    }
-}
+#define SCREEN_WIDTH 80  // Define the width of the terminal screen
 
 int main() {
     char ch;
-    char rfid[256];
-    SDB *hptr = NULL; 
-    read_records_from_file(&hptr);
+
     do {
         system("clear");
         printf("\n");
-        print_centered("+-------------------------------+");
-        print_centered("| STUDENT RECORD MENU           |");
-        print_centered("+-------------------------------+");
-        print_centered("| a/A : Add New Record          |");
-        print_centered("| s/S : Scan the student        |");
-        print_centered("| e/E : Exit                    |");
-        print_centered("+-------------------------------+");
-        print_centered(" Enter Your Choice: ");
+
+        int padding;
+
+        padding = (SCREEN_WIDTH - 31) / 2; // Adjust padding for different lengths
+        printf("%*s+-------------------------------+\n", padding, "");
+        printf("%*s|      STUDENT RECORD MENU      |\n", padding, "");
+        printf("%*s+-------------------------------+\n", padding, "");
+        printf("%*s| a/A : Add New Record          |\n", padding, "");
+        printf("%*s| s/S : Scan the Student        |\n", padding, "");
+        printf("%*s| e/E : Exit                    |\n", padding, "");
+        printf("%*s+-------------------------------+\n", padding, "");
+        printf("%*s Enter Your Choice: ", padding, "");
 
         scanf(" %c", &ch);
+        getchar(); // Consume newline character
 
         switch (tolower(ch)) {
-            case 's':
-                strncpy(rfid, scanning(), 13);
-                rfid[12] = '\0';
-                printf("RFID - %s\n", rfid);
-		sleep(1);
-                student(&hptr, rfid);
-                break;
             case 'a':
-                print_centered("\n\t\t Add your Data :\n\n");
-                add_new_record(&hptr);
-                display_students_list(hptr);
+                padding = (SCREEN_WIDTH - 20) / 2;
+                printf("\n%*s Add your Data :\n\n", padding, "");
                 break;
             case 'e':
-                Save_record(hptr);
-                usleep(50000);
-                print_centered("Exitting...");
-                usleep(50000);
+                padding = (SCREEN_WIDTH - 9) / 2;
+                printf("\n%*s Exiting...\n", padding, "");
                 exit(0);
             default:
-                print_centered("Invalid option");
+                padding = (SCREEN_WIDTH - 19) / 2;
+                printf("\n%*s Invalid option\n", padding, "");
                 break;
         }
-        
-        print_centered("Do you want to try again? (y/n): ");
+
+        padding = (SCREEN_WIDTH - 32) / 2;
+        printf("\n%*s Do you want to try again? (y/n): ", padding, "");
         scanf(" %c", &ch);
+        getchar(); // Consume newline character
     } while (tolower(ch) == 'y');
 
     return 0;
 }
-
-void student(SDB **ptr, char *rfid) {
-    char ch;
-    do {
-        if (*ptr == NULL) {
-            print_centered("\n\tNo records found.\n");
-            return;
-        }
-        system("clear");
-        if (display_student_list(*ptr, rfid) != 1) return;
-
-        printf("\n");
-        print_centered("+-------------------------------+");
-        print_centered("| STUDENT RECORD MENU           |");
-        print_centered("+-------------------------------+");
-        print_centered("| m/M : Modify A Record         |");
-        print_centered("| d/D : Delete A Record         |");
-        print_centered("| e/E : Exit                    |");
-        print_centered("+-------------------------------+");
-        print_centered(" Enter Your Choice: ");
-
-        scanf(" %c", &ch);
-
-        switch (tolower(ch)) {
-            case 'm':
-                print_centered("\t Modify A Record");
-                Modify_record(ptr, rfid);
-                system("clear");
-                display_student_list(*ptr, rfid);
-                break;
-            case 'd':
-                print_centered("\t Delete A Record");
-                Delete_record(ptr, rfid);
-                break;
-            case 'e':
-                Save_record(*ptr);
-                usleep(50000);
-                print_centered("\t Exitting...");
-                usleep(50000);
-                return;
-            default:
-                print_centered("\t Invalid option");
-                break;
-        }
 
         print_centered("\t Do you want to stay here? (y/n): ");
         scanf(" %c", &ch);
